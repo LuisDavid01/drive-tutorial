fc1ef8e586d4c0ef7093cc2ac86d7add47e10f11
Luis David <luisdavidmirandav12@gmail.com>
2025-08-11 14:39:02 -0600

    folders now manage in specific routes

---
+++ src/app/f/[folderId]/page.tsx
@@ -0,0 +1,25 @@
+import { db } from "~/server/db";
+import DriveContents from "../../drive-contents";
+import { files as fileSchema, folders as folderSchema } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
+
+
+export default async function GoogleDriveClone( props: 
+    {params : Promise<{
+        folderId: string
+    }>}
+) {
+
+    const params = await props.params
+
+    const parsedFolderId = parseInt(params.folderId);
+  if (isNaN(parsedFolderId)) {
+    return <div>Invalid folder ID</div>;
+  }
+    
+  const files = await db.select().from(fileSchema);
+  const folders = await db.select()
+  .from(folderSchema)
+  .where(eq(folderSchema.parent, parsedFolderId));
+  return(<DriveContents files={files} folders={folders} />)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { db } from "~/server/db";
-import DriveContents from "./drive-contents";
-import { files as fileSchema, folders as folderSchema } from "~/server/db/schema";
 
 
-export default async function GoogleDriveClone() {
-  const files = await db.select().from(fileSchema);
-  const folders = await db.select().from(folderSchema);
-  return(<DriveContents files={files} folders={folders} />)
+export default  function GoogleDriveClone() {
+  return <div>This is the home page</div>
 }
 
