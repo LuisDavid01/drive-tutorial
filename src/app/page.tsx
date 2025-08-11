e30023c3e7017a5f5f290e6c8edd37ce0fcf9f0b
Luis David <luisdavidmirandav12@gmail.com>
2025-08-11 10:41:09 -0600

    feat: folders component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@
 
 import { useMemo, useState } from "react"
 import {  mockFiles, mockFolders } from "../lib/mock-data"
-import { Folder, FileIcon, Upload, ChevronRight } from "lucide-react"
-import Link from "next/link"
+import { Upload, ChevronRight } from "lucide-react"
 import { Button } from "~/components/ui/button"
 import { FileRow, FolderRow } from "./folder-row"
 
